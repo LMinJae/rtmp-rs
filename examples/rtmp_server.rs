cf9188e36d8f68c388a858db0ad9a9361a64fe49
Min-Jae, Lee <lee.min.jae.96@gmail.com>
2022-02-17 18:33:04 +0900

    Impl. Chunk basic header processing and Set Chunk Size
    5.3. Chunking
    5.4.1. Set Chunk Size (1)

--- examples/rtmp_server.rs
+++ examples/rtmp_server.rs
@@ -2,6 +2,7 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 use rtmp::handshake::{Handshake, HandshakeError};
+use rtmp::chunk::{Chunk, MessageData};
 
 fn handshaking(mut stream: &TcpStream) {
     println!("Handshake Begin");
@@ -33,8 +34,53 @@ fn handshaking(mut stream: &TcpStream) {
     println!("Handshake Done");
 }
 
+
+fn chunk_process(mut stream: &TcpStream) {
+    let mut ctx = Chunk::new();
+
+    let mut buf = vec!(0_u8, 128);
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                return
+            }
+            Ok(n) => {
+            unsafe { buf.set_len(n) }
+
+            ctx.buffering(buf.as_slice());
+            }
+            _ => {}
+        }
+
+        loop {
+            match ctx.poll() {
+                Ok(None) => {
+                    break
+                }
+                Ok(Some(chunk)) => {
+                    match chunk.msg {
+                        MessageData::SetChunkSize { chunk_size } => {
+                            if chunk_size as usize > buf.capacity() {
+                                buf.reserve_exact((chunk_size as usize) - buf.capacity());
+                            }
+                },
+                _ => {}
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error while chunk processing: {:?}", e);
+                    return
+                }
+            }
+        }
+
+        unsafe { buf.set_len(buf.capacity()) };
+    }
+}
+
 fn handle_client(mut stream: TcpStream) {
     handshaking(&mut stream);
+    chunk_process(&mut stream);
 }
 
 fn main() -> std::io::Result<()> {
---
+++ src/chunk.rs
@@ -0,0 +1,193 @@
+use std::collections::{HashMap, VecDeque};
+
+use bytes::{Buf};
+
+#[derive(Debug)]
+pub enum ChunkError {
+    UnknownType3,
+}
+
+enum State {
+    Uninitialized,
+    BasicHeader {
+        fmt: u8,
+        cs_id: u16,
+    },
+    ChunkMessageHeader {
+        cs_id: u16,
+    }
+}
+
+pub struct Chunk {
+    state: State,
+    buffer: VecDeque<u8>,
+    cs_headers: HashMap<u16, HeaderMessage>,
+}
+
+impl Chunk {
+    pub fn new() -> Self {
+        Chunk {
+            state: State::Uninitialized,
+            buffer: VecDeque::with_capacity(128),
+            cs_headers: HashMap::new(),
+        }
+    }
+
+    pub fn buffering(&mut self, buf: &[u8]) {
+        self.buffer.extend(buf)
+    }
+
+    pub fn poll(&mut self) -> Result<Option<Message>, ChunkError> {
+        loop {
+            match self.state {
+                State::Uninitialized => {
+                    if 0 == self.buffer.len() {
+                        return Ok(None)
+                    }
+
+                    let n = self.buffer.get_u8();
+                    self.state = State::BasicHeader {
+                        fmt: n >> 6,
+                        cs_id: {
+                            match 0b00111111 & n {
+                                0b111111 => {
+                                    self.buffer.get_u16() + 64
+                                },
+                                0b000000 => {
+                                    (self.buffer.get_u8() as u16) + 64
+                                },
+                                n => {
+                                    n as u16
+                                },
+                            }
+                        },
+                    };
+                },
+                State::BasicHeader { fmt, cs_id } => {
+                    /*  */ if 0 == fmt && 11 > self.buffer.len() {
+                        return Ok(None)
+                    } else if 1 == fmt && 7 > self.buffer.len() {
+                        return Ok(None)
+                    } else if 2 == fmt && 3 > self.buffer.len() {
+                        return Ok(None)
+                    }
+
+                    if 0b11 == fmt {
+                        self.state = State::ChunkMessageHeader {
+                            cs_id,
+                        }
+                    } else {
+                        let timestamp = {
+                            let mut n = 0;
+                            for x in self.buffer.drain(0..3) {
+                                n = (n << 8) | (x as u32);
+                            }
+                            n
+                        };
+
+                        if 0b10 == fmt {
+                            self.cs_headers.insert(cs_id, HeaderMessage {
+                                timestamp,
+                            length: None,
+                            type_id: None,
+                            stream_id: None
+                            })
+                        } else {
+                            let length = {
+                                let mut n = 0;
+                                for x in self.buffer.drain(0..3) {
+                                    n = (n << 8) | (x as u32);
+                                }
+                                n
+                        };
+                            let type_id = self.buffer.get_u8();
+
+                            if 0b00 != fmt {
+                                self.cs_headers.insert(cs_id, HeaderMessage {
+                                    timestamp,
+                                    length: Some(length),
+                                    type_id: Some(type_id),
+                                    stream_id: None,
+                                })
+                            } else {
+                                self.cs_headers.insert(cs_id, HeaderMessage {
+                                    timestamp,
+                                    length: Some(length),
+                                    type_id: Some(type_id),
+                                    stream_id: Some(self.buffer.get_u32())
+                                })
+                            }
+                        };
+
+                        self.state = State::ChunkMessageHeader {
+                            cs_id,
+                        }
+                    }
+                },
+                State::ChunkMessageHeader { cs_id } => {
+                    let header_msg = if let Some(header_msg) = self.cs_headers.get(&cs_id) {
+                        header_msg
+                    } else {
+                        return Err(ChunkError::UnknownType3)
+                    };
+
+                    let timestamp = header_msg.timestamp;
+                    let stream_id = header_msg.stream_id;
+
+                    if let Some(length) = header_msg.length {
+                        if length as usize > self.buffer.len() {
+                            return Ok(None)
+                        }
+                    }
+
+                    match header_msg.type_id {
+                        Some(message_types::SET_CHUNK_SIZE) => {
+                            let chunk_size = self.buffer.get_i32();
+
+                            self.state = State::Uninitialized;
+                            self.cs_headers.remove(&cs_id);
+
+                            if chunk_size as usize > self.buffer.capacity() {
+                                self.buffer.reserve_exact((chunk_size as usize) - self.buffer.capacity());
+                            }
+                            return Ok(Some(Message {
+                                cs_id,
+                                timestamp,
+                                stream_id,
+                                msg: MessageData::SetChunkSize { chunk_size },
+                            }))
+                        }
+                _ => {}
+            }
+        }
+    }
+        }
+    }
+}
+
+pub struct Message {
+    pub cs_id: u16,
+    pub timestamp: u32,
+    pub stream_id: Option<u32>,
+    pub msg: MessageData,
+}
+
+pub enum MessageData {
+    SetChunkSize {
+        chunk_size: i32,
+    },
+    Unknown,
+}
+
+// 5.3.1.2. Chunk Message Header
+pub struct HeaderMessage {
+    pub timestamp: u32,
+    pub length: Option<u32>,
+    pub type_id: Option<u8>,
+    pub stream_id: Option<u32>,
+}
+
+mod message_types {
+    // 5.4.1. Set Chunk Size (1)
+    pub const SET_CHUNK_SIZE                : u8 =  1;
+}
